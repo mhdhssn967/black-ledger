f5fa9baf908b85ff3a63e93c4b6fe25b8c4d49ee
mhdhssn967 <mhdhssn967@gmail.com>
2026-01-06 20:11:51 +0530

    build added

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { UserProvider } from './context/UserContext'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  immediate: true,
+})
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
